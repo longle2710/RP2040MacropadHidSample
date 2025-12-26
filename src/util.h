611c804c5e4ccb820159b4a1ec5b1c8b5c0620bf
Long <p.long351@gmail.com>
2025-12-27 06:54:31 +0700

    Update light configuration

--- src/util.h
+++ src/util.h
@@ -9,43 +9,29 @@
 // Timing
 #define LAMPARRAY_UPDATE_INTERVAL   10000                               // 10 ms update interval, in us
 #define LAMP_UPDATE_RATE_MS         LAMPARRAY_UPDATE_INTERVAL / 1000
-
-// OLED
-#define OLED_CS                     22
-#define OLED_RESET                  23
-#define OLED_DC                     24
-#define OLED_SCLK                   26
-#define OLED_MOSI                   27
-#define OLED_MISO                   28
-
-// Speaker
-#define SPEAKER_SHUTDOWN            14
-#define SPEAKER                     16
+#define AUTONOMOUS_LIGHTING_INTERVAL_MS 5000                            // 5 second interval for autonomous lighting
 
 // Neopixel                         
 #define NEOPIXEL                    19
 #define NEOPIXEL_PIO                pio0
 #define NEOPIXEL_BITRATE            800000
-#define NEOPIXEL_COUNT              12
+#define NEOPIXEL_COUNT              2
 
 // LampArray Attributes
 #define LAMPARRAY_LAMP_COUNT        NEOPIXEL_COUNT
-#define LAMPARRAY_WIDTH             59000    // 5.9 cm 
-#define LAMPARRAY_HEIGHT            104000   // 10.4 cm
+#define LAMPARRAY_WIDTH             800000    // 5.9 cm 
+#define LAMPARRAY_HEIGHT            300000   // 10.4 cm
 #define LAMPARRAY_DEPTH             25000    // 2.5 cm
-#define LAMPARRAY_KIND              1        // LampArrayKindKeyboard
+#define LAMPARRAY_KIND              4        // Peripheral Device
 
 // Lamp Attributes
 static Position LampPositions[NEOPIXEL_COUNT] = {
-    {10500, 30000, 0}, {29500, 30000, 0}, {48500, 30000, 0}, 
-    {10500, 49500, 0}, {29500, 49500, 0}, {48500, 49500, 0}, 
-    {10500, 69000, 0}, {29500, 69000, 0}, {48500, 69000, 0}, 
-    {10500, 88500, 0}, {29500, 88500, 0}, {48500, 88500, 0}
+    {0, 0, 0}, {800000, 300000, 0}
 };
 
 // Misc.
 #define RED_LED                     13
-#define AUTONOMOUS_LIGHTING_COLOR   (LampColor){0, 255, 0}
+#define AUTONOMOUS_LIGHTING_COLOR   (LampColor){255, 0, 0}
 #define AUTONOMOUS_LIGHTING_EFFECT  BLINK
 
 #endif
